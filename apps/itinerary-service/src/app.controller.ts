ec807dc30184dfdbf315e76065470448d46f9e91
allanvarghese567-lab <allanvarghese567@gmail.com>
2026-05-17 18:38:40 +0530

    Refactor AppController constructor formatting

--- apps/itinerary-service/src/app.controller.ts
+++ apps/itinerary-service/src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get
+} from '@nestjs/common';
+
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse
+} from '@nestjs/swagger';
+
 import { AppService } from './app.service';
+
+@ApiTags('App')
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+
+  constructor(
+    private readonly appService: AppService
+  ) {}
 
   @Get()
+
+  @ApiOperation({
+    summary: 'Get application status'
+  })
+
+  @ApiResponse({
+    status: 200,
+    description: 'Application is running successfully'
+  })
+
   getHello() {
     return this.appService.getHello();
   }
+
 }
