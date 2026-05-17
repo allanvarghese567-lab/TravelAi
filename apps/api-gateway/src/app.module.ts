a7e75763c152984db7f0cecaf14836223aa170f6
allanvarghese567-lab <allanvarghese567@gmail.com>
2026-05-17 18:56:54 +0530

    Refactor app.module.ts to include additional modules

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,10 +1,46 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AppController }
+from './app.controller';
+
+import { AppService }
+from './app.service';
+
+import { AuthModule }
+from './modules/auth/auth.module';
+
+import { TripModule }
+from './modules/trip/trip.module';
+
+import { RecommendationModule }
+from './modules/recommendation/recommendation.module';
+
+import { LocationModule }
+from './modules/location/location.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+
+  imports: [
+
+    AuthModule,
+    TripModule,
+    RecommendationModule,
+    LocationModule,
+
+  ],
+
+  controllers: [
+
+    AppController
+
+  ],
+
+  providers: [
+
+    AppService
+
+  ],
+
 })
+
 export class AppModule {}
