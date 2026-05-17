2747827ed322e1676f3f56933f4970ee61dda202
allanvarghese567-lab <allanvarghese567@gmail.com>
2026-05-17 18:44:03 +0530

    Refactor AppController and add Swagger decorators
    Refactor constructor formatting and add Swagger decorators for API documentation.

--- apps/trip-service/src/app.controller.ts
+++ apps/trip-service/src/app.controller.ts
@@ -1,12 +1,41 @@
 import { Controller, Get } from '@nestjs/common';
+
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
+
 import { AppService } from './app.service';
 
+@ApiTags('Application')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+
+  constructor(
+    private readonly appService: AppService,
+  ) {}
 
   @Get()
+
+  @ApiOperation({
+    summary: 'Application health endpoint',
+    description: 'Checks whether the backend is running',
+  })
+
+  @ApiResponse({
+    status: 200,
+    description: 'Backend running successfully',
+    schema: {
+      example: {
+        success: true,
+        message: 'Travel AI Platform Running',
+      },
+    },
+  })
+
   getHello() {
     return this.appService.getHello();
   }
+
 }
