9d80aca790cacfd6843d9e144d8ea3a6ffa3ce04
allanvarghese567-lab <allanvarghese567@gmail.com>
2026-05-17 19:27:19 +0530

    Update main.ts

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,9 +1,48 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory }
+from '@nestjs/core';
+
+import {
+  SwaggerModule,
+  DocumentBuilder,
+} from '@nestjs/swagger';
+
+import { AuthModule }
+from './auth.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+
+  const app =
+    await NestFactory.create(
+      AuthModule,
+    );
+
+  const config =
+    new DocumentBuilder()
+
+      .setTitle(
+        'Auth Service',
+      )
+
+      .setVersion('1.0')
+
+      .addBearerAuth()
+
+      .build();
+
+  const document =
+    SwaggerModule.createDocument(
+      app,
+      config,
+    );
+
+  SwaggerModule.setup(
+    'api',
+    app,
+    document,
+  );
+
+  await app.listen(3001);
+
 }
+
 bootstrap();
