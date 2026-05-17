4714fd0c4514083fd27552bf100d99a7ec7b10dd
allanvarghese567-lab <allanvarghese567@gmail.com>
2026-05-18 00:31:34 +0530

    commit

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-// import {ServiceConstants} from '@shared/index';
+import {ServiceConstants} from '@shared/index';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
--- apps/api-gateway/src/middleware/proxy.middleware.ts
+++ apps/api-gateway/src/middleware/proxy.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import{ServiceConstant} from '@shared/index';
 
 export function authMiddleware(req: Request, res: Response, next: NextFunction) {
   const publicPaths = ['/auth/login', '/auth/register', '/docs'];
